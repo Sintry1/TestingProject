88e2e5aa1503abd448c74fb832232d9e64c8c9c3
Kwandes <bz.jasiek@gmail.com>
2023-01-14 22:01:41 +0100

    test: 💍 Adjust tests structure

--- apps/frontend-e2e/src/e2e/app.cy.ts
+++ apps/frontend-e2e/src/e2e/app.cy.ts
@@ -1,62 +1,7 @@
-import { getGreeting } from '../support/app.po';
-
 describe('frontend', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    //getGreeting().contains('Welcome frontend');
+  it('should open the website', () => {
+    cy.title().should('equal', 'Omnihost Systems');
   });
 });
-
-describe('Long term page', () => {
-  beforeEach(() => cy.visit('/long-term'));
-
-  it('Does not contain data', () => {
-    cy.get('[data-cy=long-term-list-entry]').should('exist')
-  })
-
-  it('should open create long term dialog', () => {
-    cy.get('[data-cy=long-term-create-dialog]').should('not.exist');
-    cy.get('[data-cy=long-term-create-dialog-button]').should('exist');
-    cy.get('[data-cy=long-term-create-dialog-button]').click()
-    cy.get('[data-cy=long-term-create-dialog]').should('exist');
-  })
-
-  it('should open an update dialog when clicking a row', () => {
-    cy.get('[data-cy=long-term-update-dialog]').should('not.exist');
-    cy.get('[data-cy=long-term-list-entry]').first().click();
-    cy.get('[data-cy=long-term-update-dialog]').should('exist');
-  })
-
-  describe('Update dialog', () => {
-    beforeEach(() => {
-      cy.get('[data-cy=long-term-list-entry]').first().click({force:true});
-    })
-
-    it('should contain the data', () => {
-      cy.get('[data-cy=long-term-update-dialog]').should('exist');
-      cy.get('[data-cy=input-field-room]').should('have.value', '925');
-    })
-
-    it('should close update dialog when clicking cancel', () => {
-      cy.get('[data-cy=long-term-update-dialog]').should('exist');
-      cy.get('[data-cy=update-dialog-cancel-button]').click({force:true})
-      cy.get('[data-cy=long-term-update-dialog]').should('not.exist');
-    })
-
-    it('should be able to change data', () => {
-      cy.get('[data-cy=long-term-update-dialog]').should('exist');
-      cy.get('[data-cy=input-field-room]').should('have.value', '925');
-
-      cy.get('[data-cy=input-field-room]').clear();
-      cy.get('[data-cy=input-field-room]').should('have.value', '');
-
-      cy.get('[data-cy=input-field-room]').type('123');
-      cy.get('[data-cy=input-field-room]').should('have.value', '123');
-    })
-  })
-})
---
+++ apps/frontend-e2e/src/e2e/long-term.cy.ts
@@ -0,0 +1,48 @@
+describe('Long term page', () => {
+  beforeEach(() => cy.visit('/long-term'));
+
+  it('should contain data', () => {
+    cy.get('[data-cy=long-term-list-entry]').should('have.length.above', 0);
+  });
+
+  it('should open create long term dialog', () => {
+    cy.get('[data-cy=long-term-create-dialog]').should('not.exist');
+    cy.get('[data-cy=long-term-create-dialog-button]').should('exist');
+    cy.get('[data-cy=long-term-create-dialog-button]').click();
+    cy.get('[data-cy=long-term-create-dialog]').should('exist');
+  });
+
+  it('should open an update dialog when clicking a row', () => {
+    cy.get('[data-cy=long-term-update-dialog]').should('not.exist');
+    cy.get('[data-cy=long-term-list-entry]').first().click();
+    cy.get('[data-cy=long-term-update-dialog]').should('exist');
+  });
+
+  describe('Update dialog', () => {
+    beforeEach(() => {
+      cy.get('[data-cy=long-term-list-entry]').first().click({ force: true });
+    });
+
+    it('should contain the data', () => {
+      cy.get('[data-cy=long-term-update-dialog]').should('exist');
+      cy.get('[data-cy=input-field-room]').should('have.value', '925');
+    });
+
+    it('should close update dialog when clicking cancel', () => {
+      cy.get('[data-cy=long-term-update-dialog]').should('exist');
+      cy.get('[data-cy=update-dialog-cancel-button]').click({ force: true });
+      cy.get('[data-cy=long-term-update-dialog]').should('not.exist');
+    });
+
+    it('should be able to change data', () => {
+      cy.get('[data-cy=long-term-update-dialog]').should('exist');
+      cy.get('[data-cy=input-field-room]').should('have.value', '925');
+
+      cy.get('[data-cy=input-field-room]').clear();
+      cy.get('[data-cy=input-field-room]').should('have.value', '');
+
+      cy.get('[data-cy=input-field-room]').type('123');
+      cy.get('[data-cy=input-field-room]').should('have.value', '123');
+    });
+  });
+});
