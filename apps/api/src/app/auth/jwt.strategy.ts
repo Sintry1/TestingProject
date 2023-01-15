a6e2af1141e501aa8c637b135eb03fd1300bac70
Kwandes <bz.jasiek@gmail.com>
2023-01-15 14:19:59 +0100

    Add handling of tokens that don't exist in the database

--- apps/api/src/app/auth/jwt.strategy.ts
+++ apps/api/src/app/auth/jwt.strategy.ts
@@ -1,14 +1,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { IJwtInfo } from '@omnihost/interfaces';
 import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { TokensService } from '../tokens/tokens.service';
 import { jwtConstants } from './constants';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  private readonly logger = new Logger(JwtStrategy.name);
+  constructor(private tokensService: TokensService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -19,13 +21,22 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
   async validate(req: Request, payload: any): Promise<IJwtInfo> {
     const token = req.get('Authorization').replace('Bearer', '').trim();
-    return {
-      payload: {
-        userId: payload.userId,
-        email: payload.email,
-        role: payload.role,
-      },
-      token,
-    };
+    try {
+      await this.tokensService.findByToken(token);
+      return {
+        payload: {
+          userId: payload.userId,
+          email: payload.email,
+          role: payload.role,
+        },
+        token,
+      };
+    } catch (error) {
+      this.logger.warn(
+        `Jwt validation failed since the token doesn't exist in the database`,
+        token
+      );
+      throw new UnauthorizedException();
+    }
   }
 }
