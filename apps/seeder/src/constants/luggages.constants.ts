6216716bbc67171c666cca4d3641072527a380d8
Kwandes <bz.jasiek@gmail.com>
2023-01-18 21:31:07 +0100

    Refactor luggage generation logic

--- apps/seeder/src/constants/luggages.constants.ts
+++ apps/seeder/src/constants/luggages.constants.ts
@@ -1,5 +1,24 @@
 import { ILuggage, LuggageType } from '@omnihost/interfaces';
 
+export const bags = [
+  '1',
+  '1',
+  '1',
+  '2',
+  '2',
+  '2',
+  '3',
+  '4',
+  '1+1',
+  '2+1',
+  '3+1',
+  '4+1',
+  '4+2',
+  '5+2',
+];
+
+export const luggageComments = ['Fragile', 'Broken wheels', 'Keep upright', 'Very heavy'];
+
 export const luggages: ILuggage[] = [
   {
     createdAt: new Date(
--- apps/seeder/src/services/luggages.service.ts
+++ apps/seeder/src/services/luggages.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { ILuggage } from '@omnihost/interfaces';
+import { ILuggage, LuggageType } from '@omnihost/interfaces';
 import { Luggage } from '@omnihost/models';
 import { Repository } from 'typeorm';
-import { luggages } from '../constants/luggages.constants';
+import { v4 as uuidv4 } from 'uuid';
+import { bellBoyInitials, luggageLocation } from '../constants/dropdown-options';
+import { bags, luggageComments } from '../constants/luggages.constants';
+import { names } from '../constants/names.constant';
+import { getRandom, getRandomBoolean, getRandomChance, getRandomInt } from './utils.service';
 
 @Injectable()
 export class LuggagesSeederService {
@@ -13,12 +17,60 @@ export class LuggagesSeederService {
   ) {}
 
   create(): Array<Promise<Luggage>> {
-    return luggages.map(async (luggage: ILuggage) => {
+    return this.generateLuggage().map(async (luggage: ILuggage) => {
       try {
         return await this.repo.save(luggage);
       } catch (error) {
         throw new Error(error);
       }
     });
+  }
+
+  generateLuggage() {
+    // Control variables
+    const entriesPerDay = 10;
+    const entriesPerDayRandomFactor = 2; // random +/- value for entires
+
+    // Setup the dates for the period of data generation
+    const startDate = new Date(Date.now());
+    startDate.setMonth(startDate.getMonth() - 6); // day six months in the past
+    const endDate = new Date(Date.now());
+    endDate.setMonth(endDate.getMonth() + 2); // day 2 months in the future
+
+    // Generate the data
+    const data: ILuggage[] = [];
+    for (let day = startDate; day <= endDate; day.setDate(day.getDate() + 1)) {
+      [LuggageType.CHECKIN, LuggageType.CHECKOUT, LuggageType.LONG_TERM].forEach((luggageType) => {
+        const entiresMultiplier = getRandomBoolean() ? -1 : 1;
+        const entries =
+          entriesPerDay + getRandomInt(0, entriesPerDayRandomFactor) * entiresMultiplier;
+        for (let i = 0; i < entries; i++) {
+          const roomReady =
+            luggageType === LuggageType.LONG_TERM ? getRandomChance(0.9) : getRandomBoolean(); // long term storage has 10% of not being completed
+          const morningDate = new Date(day.setHours(getRandomInt(6, 12), getRandomInt(0, 60)));
+          const eveningDate = new Date(day.setHours(getRandomInt(13, 22), getRandomInt(0, 60)));
+          data.push({
+            luggageId: uuidv4(),
+            luggageType: luggageType,
+            room: getRandomInt(100, 500).toString(), // TODO - replace with the rooms array once it is implemented
+            name: getRandom(names),
+            bags: getRandom(bags),
+            tagNr: getRandomInt(1000, 4000).toString(),
+            location: getRandom(luggageLocation),
+            comments: getRandomChance(0.4) ? getRandom(luggageComments) : null,
+            roomReady: roomReady,
+            createdAt: morningDate,
+            arrivalTime: morningDate,
+            dateNeeded: eveningDate,
+            bbLr: getRandom(bellBoyInitials), // who put it in luggage room
+            bbOut: roomReady ? getRandom(bellBoyInitials) : null, // who put it in the quest's room or gave it to the guest
+            bbDown: roomReady ? getRandom(bellBoyInitials) : null, // who brought the luggage from luggage room from the guest's room
+            completedAt: roomReady ? eveningDate : null,
+          });
+        }
+      });
+    }
+
+    return data;
   }
 }
