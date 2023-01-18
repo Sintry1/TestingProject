5504eda951fa7498a30d29accdac43628ff64f09
CristiPV <cristi.valentin29@gmail.com>
2023-01-18 12:08:24 +0100

    Update apps/frontend/src/app/utils/date.util.ts
    Co-authored-by: Kwandes <22862227+Kwandes@users.noreply.github.com>

--- apps/frontend/src/app/utils/date.util.ts
+++ apps/frontend/src/app/utils/date.util.ts
@@ -21,7 +21,6 @@ export function toDateInputString(date: Date) {
 }
 
 export function toDatetimeInputString(date: Date) {
-  console.log(date, typeof date);
   const { day, month, year, hour, minute } = getDateParts(date);
   const datetimeString = `${year}-${month}-${day}T${hour}:${minute}`;
 
