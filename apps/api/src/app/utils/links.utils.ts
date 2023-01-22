5c87adad2e9d3d3618cca6e30f11f71c5738630f
Kwandes <bz.jasiek@gmail.com>
2023-01-22 14:46:14 +0100

    Fix typos in domain links

--- apps/api/src/app/utils/links.utils.ts
+++ apps/api/src/app/utils/links.utils.ts
@@ -10,11 +10,11 @@ export function getFrontendUrlFromEnv(): string {
     case 'PRODUCTION':
       return 'https://dangleterre.omnihost.app';
     case 'STAGING':
-      return 'https://staging.dangleterre-omnihost.app';
+      return 'https://staging.dangleterre.omnihost.app';
     case 'DEMO':
-      return 'https://demo.dangleterre-omnihost.app';
+      return 'https://demo.dangleterre.omnihost.app';
     case 'TEST':
-      return 'https://test.dangleterre-omnihost.app';
+      return 'https://test.dangleterre.omnihost.app';
     default:
       return 'http://localhost:4200';
   }
