8bf9e9da8823226bb4f07582f6ebd45e9469d0cf
CristiPV <cristi.valentin29@gmail.com>
2023-01-22 17:50:31 +0100

    add announcement model

---
+++ libs/models/src/lib/announcement.entity.ts
@@ -0,0 +1,31 @@
+import { ApiModelProperty } from '@nestjs/swagger/dist/decorators/api-model-property.decorator';
+import { IAnnouncement } from '@omnihost/interfaces';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Base } from './base.entity';
+
+@Entity('announcements')
+export class Announcement extends Base implements IAnnouncement {
+  @ApiModelProperty({ example: '80a0d2ac-ec67-482e-9017-909b72a74be7' })
+  @PrimaryGeneratedColumn('uuid')
+  announcementId!: string;
+
+  @ApiModelProperty({ example: 'Tour de France' })
+  @Column({ nullable: true })
+  title?: string;
+
+  @ApiModelProperty({ example: "Tour de France is so cool, yo. Please don't miss it." })
+  @Column({ nullable: true })
+  comments?: string;
+
+  @ApiModelProperty({ example: new Date() })
+  @Column({ nullable: true })
+  showTo?: Date | null;
+
+  @ApiModelProperty({ example: new Date() })
+  @Column({ nullable: true })
+  showFrom?: Date | null;
+
+  @ApiModelProperty({ example: ['tourDeFrance.pdf', 'selfieFromParis.png'] })
+  @Column({ default: [] })
+  files!: string[];
+}
