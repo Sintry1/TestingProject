fdb3da230c310137f8df9fe57945195b11245815
Frederik-Vagner <70664117+Frederik-Vagner@users.noreply.github.com>
2023-01-25 21:52:21 +0100

    Create sort buttons

--- apps/frontend/src/app/pages/announcements/announcements.component.ts
+++ apps/frontend/src/app/pages/announcements/announcements.component.ts
@@ -3,25 +3,34 @@ import { CreateAnnouncementDialogComponent } from './create-announcement-dialog/
 import { MatDialog } from '@angular/material/dialog';
 import { UpdateAnnouncementDialogComponent } from './update-announcement-dialog/update-announcement-dialog.component';
 
+enum AnnouncementStatus {
+  FUTURE = 'Future',
+  ACTIVE = 'Active',
+  EXPIRED = 'Expired',
+}
+
 @Component({
   selector: 'frontend-announcements',
   templateUrl: './announcements.component.html',
-  styleUrls: ['../../../assets/styles/table.scss'],
+  styleUrls: ['../../../assets/styles/table.scss', './announcements.component.scss'],
 })
 export class AnnouncementsComponent {
   displayDate = new Date();
   isLoading = false;
   displayedColumns: string[] = ['title', 'comments', 'showFrom', 'showTo', 'images', 'status'];
-  dataSource = ELEMENT_DATA;
+  displayAnnouncementList: IAnnouncement[] = [];
+  showFuture = true;
+  showActive = true;
+  showExpired = false;
+  announcementList: IAnnouncement[] = MOCK_DATA;
+  
 
-  announcementColumns = ['title', 'comment', 'shownFrom', 'showTo', 'images', 'status'];
+  announcementColumns = ['title', 'comment', 'showFrom', 'showTo', 'images', 'status'];
 
-  constructor(
-    private dialog: MatDialog,
-  ){}
+  constructor(private dialog: MatDialog) {}
 
-  createAnnouncement(): void{
-  this.dialog.open(CreateAnnouncementDialogComponent, { width: '800px' });
+  createAnnouncement(): void {
+    this.dialog.open(CreateAnnouncementDialogComponent, { width: '800px' });
   }
 
   editAnnouncement(): void {
@@ -29,9 +38,61 @@ export class AnnouncementsComponent {
       width: '800px',
     });
   }
+
+  getStatus(showFrom: Date, showTo: Date): string {
+    const today = new Date();
+    // FUTURE
+    if (showFrom > today) {
+      return AnnouncementStatus.FUTURE;
+      // True if showTo is in the past (showTo < date.now())
+
+      // ACTIVE
+    } else if (showFrom <= today && showTo > today) {
+      return AnnouncementStatus.ACTIVE;
+    } else {
+
+      // EXPIRED
+      return AnnouncementStatus.EXPIRED;
+    }
+  }
+
+  updateList(): void {
+    this.displayAnnouncementList = [];
+    console.log("works");
+
+     //check to display future
+     if(this.showFuture) {
+      console.log("adding future");
+      this.displayAnnouncementList = this.displayAnnouncementList.concat(
+        this.announcementList.filter((item) => this.getStatus(item.showFrom, item.showTo) === AnnouncementStatus.FUTURE)
+      );
+      console.log("Future items have been added");
+    }
+    
+    // check to display active
+    if(this.showActive) {
+      console.log("adding active");
+      this.displayAnnouncementList = this.displayAnnouncementList.concat(
+        this.announcementList.filter((item) => this.getStatus(item.showFrom, item.showTo) !== AnnouncementStatus.ACTIVE)
+      );
+      console.log("Active items have been added");
+    }
+
+    //check to display expired
+    if(this.showExpired) {
+      console.log("adding future");
+      this.displayAnnouncementList = this.displayAnnouncementList.concat(
+        this.announcementList.filter((item) => this.getStatus(item.showFrom, item.showTo) === AnnouncementStatus.EXPIRED)
+      );
+      console.log("Expired items have been added");
+    }
+   console.log(this.displayAnnouncementList);
+   
+  }
+  
 }
 
-export interface PeriodicElement {
+export interface IAnnouncement {
   title: string;
   comments: string;
   showFrom: Date;
@@ -40,50 +101,50 @@ export interface PeriodicElement {
   status: string;
 }
 
-const today = new Date()
-const twoDays = new Date(today)
-const threeDays = new Date(today)
-const minusOneDay = new Date(today)
-const minusTwoDays = new Date(today)
-minusTwoDays.setDate(minusTwoDays.getDate() - 2)
-minusOneDay.setDate(minusOneDay.getDate() - 1)
-twoDays.setDate(twoDays.getDate() + 2)
-threeDays.setDate(threeDays.getDate() + 3)
+const today = new Date();
+const twoDays = new Date(today);
+const threeDays = new Date(today);
+const minusOneDay = new Date(today);
+const minusTwoDays = new Date(today);
+minusTwoDays.setDate(minusTwoDays.getDate() - 2);
+minusOneDay.setDate(minusOneDay.getDate() - 1);
+twoDays.setDate(twoDays.getDate() + 2);
+threeDays.setDate(threeDays.getDate() + 3);
 
-const ELEMENT_DATA: PeriodicElement[] = [
+const MOCK_DATA: IAnnouncement[] = [
   // Expired yesterday (expired)
   {
     title: 'Department update (past)',
     comments: 'Updated opening hours for each department',
     showFrom: minusTwoDays,
-    shownTo: minusOneDay,
+    showTo: minusOneDay,
     images: 'image',
     status: 'Critical',
   },
   // Currently shown (active)
   {
-    title: 'Department update',
+    title: 'Department update (active)',
     comments: 'Updated opening hours for each department',
     showFrom: today,
-    shownTo: twoDays,
+    showTo: twoDays,
     images: 'image',
     status: 'Critical',
   },
   // Currently shown (active)
   {
-    title: 'Staff update',
+    title: 'Staff update (active)',
     comments: 'A big welcome to all the new employees',
     showFrom: today,
-    shownTo: threeDays,
+    showTo: threeDays,
     images: 'image',
     status: 'Need to know',
   },
   // Active in two days (future)
   {
-    title: 'Tour De France update',
+    title: 'Tour De France update (future)',
     comments: 'A guide on how the race affect the hotel',
     showFrom: twoDays,
-    shownTo: threeDays,
+    showTo: threeDays,
     images: 'image',
     status: 'Nice to know',
   },
