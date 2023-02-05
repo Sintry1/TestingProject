b588f674254c9f58c084787b362cceac272c2ab5
CristiPV <cristi.valentin29@gmail.com>
2023-02-05 11:30:53 +0100

    add seeder annoucement seed service

---
+++ apps/seeder/src/constants/announcements.constant.ts
@@ -0,0 +1,14 @@
+export const announcements = [
+  { title: 'Christmas Incoming!', comments: 'Christmas gifts under the tree in the lobby!' },
+  { title: 'Standard Operating procedures', comments: 'Operator' },
+  {
+    title: 'Tsunami coming to Copenhagen?',
+    comments: 'Tsunami incoming, water level rising! Quick, sell your houses to Aquaman.',
+  },
+  { title: 'Rat plagues everywhere!', comments: 'Rats everywhere.' },
+  { title: 'Public transport plan', comments: null },
+  {
+    title: 'Changing opening hours',
+    comments: 'Opening hours of various departments',
+  },
+];
---
+++ apps/seeder/src/services/announcements.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { IAnnouncement } from '@omnihost/interfaces';
+import { Announcement } from '@omnihost/models';
+import { Repository } from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
+import { announcements } from '../constants/announcements.constant';
+import * as fs from 'fs';
+import * as path from 'path';
+import { getRandomInt, uploadFileToLinode } from './utils.service';
+
+@Injectable()
+export class AnnouncementsSeederService {
+  constructor(
+    @InjectRepository(Announcement)
+    private readonly repo: Repository<Announcement>
+  ) {}
+
+  create(): Array<Promise<Announcement>> {
+    // The file that will be uploaded to Linode
+    const fileBuffer = fs.readFileSync(path.join(__dirname, '/assets/IMG_9549.jpg'));
+
+    return this.generate().map(async (announcement: IAnnouncement) => {
+      try {
+        await uploadFileToLinode(
+          fileBuffer,
+          `${announcement.announcementId}${announcement.files[0]}`
+        );
+        return await this.repo.save(announcement);
+      } catch (error) {
+        throw new Error(error);
+      }
+    });
+  }
+
+  generate() {
+    // Setup the dates for the period of data generation
+    const startDate = new Date(Date.now());
+    startDate.setMonth(startDate.getMonth() - 1); // six months in the past
+    const endDate = new Date(Date.now());
+    endDate.setMonth(endDate.getMonth() + 2); // 2 months in the future
+
+    // Generate the data
+    const data: IAnnouncement[] = [];
+    announcements.forEach((announcement) => {
+      const showFrom = new Date(startDate);
+      showFrom.setDate(showFrom.getMonth() - getRandomInt(1, 6));
+      showFrom.setDate(showFrom.getDate() + getRandomInt(1, 28));
+
+      const showTo = new Date(startDate);
+      showTo.setDate(showTo.getMonth() - getRandomInt(1, 6));
+      showTo.setDate(showTo.getDate() + getRandomInt(1, 28));
+
+      data.push({
+        announcementId: uuidv4(),
+        comments: announcement.comments,
+        title: announcement.title,
+        showFrom,
+        showTo,
+        files: ['IMG_9549.jpg'],
+      });
+    });
+
+    return data;
+  }
+}
