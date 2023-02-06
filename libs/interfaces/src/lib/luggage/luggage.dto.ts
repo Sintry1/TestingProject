b868207ebfd50ac6bc5b06bcc4a0b641503f7f52
CristiPV <cristi.valentin29@gmail.com>
2023-02-06 15:18:37 +0100

    add files to the luggage interfaces and dtos

--- libs/interfaces/src/lib/luggage/luggage.dto.ts
+++ libs/interfaces/src/lib/luggage/luggage.dto.ts
@@ -1,6 +1,12 @@
 import { ApiModelProperty } from '@nestjs/swagger/dist/decorators/api-model-property.decorator';
 import { IsDateString, IsEnum, IsNotEmpty, IsOptional } from 'class-validator';
-import { ICreateLuggageRequest, IUpdateLuggageRequest } from './luggage.interface';
+import { CompletedAtResponse } from '../base.dto';
+import {
+  ICreateLuggageRequest,
+  IGetLuggageByIdResponse,
+  ILuggage,
+  IUpdateLuggageRequest,
+} from './luggage.interface';
 import { LuggageType } from './luggageType.enum';
 
 export class CreateLuggageRequest implements ICreateLuggageRequest {
@@ -63,6 +69,11 @@ export class CreateLuggageRequest implements ICreateLuggageRequest {
   @IsOptional()
   @IsDateString()
   completedAt?: Date;
+
+  @ApiModelProperty({ type: 'array', items: { type: 'string', format: 'binary' }, required: true })
+  @IsOptional()
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  files?: any = [];
 }
 
 export class UpdateLuggageRequest implements IUpdateLuggageRequest {
@@ -125,4 +136,76 @@ export class UpdateLuggageRequest implements IUpdateLuggageRequest {
   @IsOptional()
   @IsDateString()
   completedAt?: Date;
+
+  @ApiModelProperty({ type: 'array', items: { type: 'string', format: 'binary' }, required: true })
+  @IsOptional()
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  files?: any = [];
+}
+
+export class DeleteLuggageResponse {
+  @ApiModelProperty({ example: 'Deleted.' })
+  message = 'Deleted.';
+}
+
+export class GetLuggageResponse extends CompletedAtResponse implements ILuggage {
+  @ApiModelProperty({ example: '123e4567-e89b-12d3-a456-426614174000' })
+  luggageId!: string;
+
+  @ApiModelProperty({ example: LuggageType.CHECKIN })
+  luggageType!: LuggageType;
+
+  @ApiModelProperty({ example: true })
+  roomReady?: boolean;
+
+  @ApiModelProperty({ example: '101' })
+  room!: string;
+
+  @ApiModelProperty({ example: 'EA' })
+  name!: string;
+
+  @ApiModelProperty({ example: new Date() })
+  @IsDateString()
+  arrivalTime?: Date;
+
+  @ApiModelProperty({ example: '3+2' })
+  bags!: string;
+
+  @ApiModelProperty({ example: 'Bikes, Ski Equipment' })
+  comments?: string;
+
+  @ApiModelProperty({ example: '1234' })
+  tagNr!: string;
+
+  @ApiModelProperty({ example: 'LR1' })
+  location!: string;
+
+  @ApiModelProperty({ example: 'EA' })
+  bbDown?: string;
+
+  @ApiModelProperty({ example: 'EA' })
+  bbLr!: string;
+
+  @ApiModelProperty({ example: 'EA' })
+  bbOut?: string;
+
+  @ApiModelProperty({ example: new Date() })
+  @IsDateString()
+  dateNeeded?: Date;
+
+  @ApiModelProperty({ example: new Date() })
+  @IsDateString()
+  completedAt?: Date;
+
+  @ApiModelProperty({ type: 'array', items: { type: 'string', format: 'binary' }, required: true })
+  files!: string[];
+}
+
+export class GetLuggageByIdResponse extends GetLuggageResponse implements IGetLuggageByIdResponse {
+  @ApiModelProperty({
+    example: [
+      'https://eu-central-1.linodeobjects.com:443/omnihost/21234.pdf?Signature=XEiYEET1C4T3I25s0I5K1IOH%2Co%3X&Expires=1670271241&AWSAccessKeyId=123456789EAEA',
+    ],
+  })
+  downloadUrls!: string[];
 }
