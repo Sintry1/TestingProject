dfc4841fb8feae817617bf41a43b400d260dcb2a
CristiPV <cristi.valentin29@gmail.com>
2023-02-06 16:06:13 +0100

    add files functionality to controller endpoints

--- apps/api/src/app/luggages/luggages.controller.ts
+++ apps/api/src/app/luggages/luggages.controller.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   Get,
@@ -8,10 +9,14 @@ import {
   Patch,
   Post,
   Query,
+  UploadedFiles,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
+import { FilesInterceptor } from '@nestjs/platform-express';
 import {
   ApiBearerAuth,
+  ApiConsumes,
   ApiCreatedResponse,
   ApiOkResponse,
   ApiOperation,
@@ -20,6 +25,7 @@ import {
 } from '@nestjs/swagger';
 import {
   CreateLuggageRequest,
+  GetLuggageResponse,
   LuggageSortOptions,
   LuggageType,
   Role,
@@ -32,6 +38,9 @@ import { Roles } from '../auth/roles.decorator';
 import { RequiredQuery } from '../decorators/required-query.decorator';
 import { toBool } from '../utils/query-params.utils';
 import { LuggagesService } from './luggages.service';
+import 'multer';
+
+const FILE_TYPES = /(png|jpg|jpeg)\b/;
 
 @ApiTags('Luggages')
 @Controller('luggages')
@@ -45,7 +54,7 @@ export class LuggagesController {
   @ApiOperation({
     summary: 'Get a list of checked in luggages for the given day.',
   })
-  @ApiOkResponse({ type: [Luggage] })
+  @ApiOkResponse({ type: [GetLuggageResponse] })
   @ApiQuery({ name: 'createdAt', required: true, example: new Date() })
   @ApiQuery({ name: 'status', required: false, example: 'true' })
   @ApiQuery({
@@ -100,21 +109,119 @@ export class LuggagesController {
     summary: 'Create a luggage entry.',
   })
   @ApiCreatedResponse({ type: Luggage })
+  @ApiConsumes('multipart/form-data')
   @HttpCode(201)
-  async createLuggage(@Body() luggageData: CreateLuggageRequest) {
-    return this.luggagesService.createLuggage(luggageData);
+  @UseInterceptors(
+    FilesInterceptor('files', 20, {
+      fileFilter(req, file, callback) {
+        const nameParts = file.originalname.split('.');
+        const fileType = nameParts[nameParts.length - 1];
+
+        if (!fileType.match(FILE_TYPES)) {
+          req.fileValidationError = `Invalid file type for file: ${file.originalname}`;
+          return callback(
+            new BadRequestException(`Invalid file type for file: ${file.originalname}`),
+            false
+          );
+        }
+
+        return callback(null, true);
+      },
+    })
+  )
+  async createLuggage(
+    @UploadedFiles()
+    files: Array<Express.Multer.File>,
+    @Body() luggageData: CreateLuggageRequest
+  ) {
+    return this.luggagesService.createLuggage(luggageData, files || []);
   }
 
   @Patch(':luggageId')
   @ApiOperation({
     summary: 'Update a luggage entry.',
   })
-  @ApiCreatedResponse({ type: Luggage })
+  @ApiOkResponse({ type: Luggage })
   @HttpCode(200)
+  @UseInterceptors(
+    FilesInterceptor('files', 20, {
+      fileFilter(req, file, callback) {
+        const nameParts = file.originalname.split('.');
+        const fileType = nameParts[nameParts.length - 1];
+
+        if (!fileType.match(FILE_TYPES)) {
+          req.fileValidationError = `Invalid file type for file: ${file.originalname}`;
+          return callback(
+            new BadRequestException(`Invalid file type for file: ${file.originalname}`),
+            false
+          );
+        }
+
+        return callback(null, true);
+      },
+    })
+  )
   async updateLuggage(
     @Param('luggageId', ParseUUIDPipe) luggageId: string,
+    @UploadedFiles()
+    files: Array<Express.Multer.File>,
     @Body() luggageData: UpdateLuggageRequest
   ) {
-    return this.luggagesService.updateLuggage(luggageId, luggageData);
+    return this.luggagesService.updateLuggage(luggageId, luggageData, files || []);
+  }
+
+  @Patch(':luggageId/files/add')
+  @ApiOperation({
+    summary: 'Add more files to a luggage.',
+  })
+  @ApiOkResponse({ type: Luggage })
+  @HttpCode(200)
+  @UseInterceptors(
+    FilesInterceptor('files', 20, {
+      fileFilter(req, file, callback) {
+        const nameParts = file.originalname.split('.');
+        const fileType = nameParts[nameParts.length - 1];
+
+        if (!fileType.match(FILE_TYPES)) {
+          req.fileValidationError = `Invalid file type for file: ${file.originalname}`;
+          return callback(
+            new BadRequestException(`Invalid file type for file: ${file.originalname}`),
+            false
+          );
+        }
+
+        return callback(null, true);
+      },
+    })
+  )
+  async addLuggageFiles(
+    @Param('luggageId', ParseUUIDPipe) luggageId: string,
+    @UploadedFiles()
+    files: Array<Express.Multer.File>
+  ) {
+    return this.luggagesService.updateLuggage(luggageId, null, files || []);
+  }
+
+  @Patch(':luggageId/files/remove')
+  @ApiOperation({
+    summary: 'Remove files from a luggage.',
+  })
+  @ApiOkResponse({ type: Luggage })
+  @HttpCode(200)
+  async removeLuggageFiles(
+    @Param('luggageId', ParseUUIDPipe) luggageId: string,
+    @Body() fileNames: string[]
+  ) {
+    return this.luggagesService.removeLuggageFiles(luggageId, fileNames);
+  }
+
+  @Patch(':luggageId/files/clear')
+  @ApiOperation({
+    summary: "Clear a luggage's files.",
+  })
+  @ApiOkResponse({ type: Luggage })
+  @HttpCode(200)
+  async clearLuggageFiles(@Param('luggageId', ParseUUIDPipe) luggageId: string) {
+    return this.luggagesService.clearLuggageFiles(luggageId);
   }
 }
