5b22e1b7748660c1f295fbc8081c353eacc2dca3
Kwandes <bz.jasiek@gmail.com>
2023-02-07 14:48:21 +0100

    Add file upload styling

--- apps/frontend/src/app/components/file-upload/file-upload.component.ts
+++ apps/frontend/src/app/components/file-upload/file-upload.component.ts
@@ -6,7 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./file-upload.component.scss'],
 })
 export class FileUploadComponent {
+  filesSelected = false;
+  allowedFileFormats = ['png', 'jpg', 'mp4'];
+
   constructor() {
     console.log('File Upload ready');
+  }
+
+  /**
+   * Get a readable version of the allowed file formats list.
+   * @returns formatted formats lists.
+   */
+  getAllowedFileFormatsAsText(): string {
+    if (this.allowedFileFormats.length == 0) {
+      return 'none';
+    }
+    if (this.allowedFileFormats.length == 1) {
+      return this.allowedFileFormats[0];
+    }
+    let text = '';
+    for (let i = 0; i < this.allowedFileFormats.length; i++) {
+      text += this.allowedFileFormats[i];
+      if (i == this.allowedFileFormats.length - 2) {
+        text += ' or ';
+      } else if (i < this.allowedFileFormats.length - 2) {
+        text += ', ';
+      }
+    }
+    return text;
   }
 }
