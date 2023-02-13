0b2a6fb174ad1c2f4d02f01509fe965781d534f4
Teodor25 <teodor_jonasson@hotmail.com>
2023-02-13 19:50:48 +0100

    feat: 🎸 Add functionality to update dialog

--- apps/frontend/src/app/pages/announcements/create-announcement-dialog/create-announcement-dialog.component.ts
+++ apps/frontend/src/app/pages/announcements/create-announcement-dialog/create-announcement-dialog.component.ts
@@ -1,10 +1,11 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { toDatetimeInputString } from '../../../utils/date.util';
 
 @Component({
-  selector: 'frontend-create-annoucement-dialog',
+  selector: 'frontend-create-announcement-dialog',
   templateUrl: './create-announcement-dialog.component.html',
   styleUrls: ['../../../../assets/styles/dialog.scss', '../announcements.component.scss'],
 })
@@ -13,18 +14,39 @@ export class CreateAnnouncementDialogComponent implements OnInit {
   isLoading = false;
 
   @ViewChild('title') titleInput!: ElementRef;
-  @ViewChild('comments') commentInput!: ElementRef;
+  @ViewChild('comments') commentsInput!: ElementRef;
   @ViewChild('showFrom') showFromInput!: ElementRef;
   @ViewChild('showTo') showToInput!: ElementRef;
 
-  constructor(private dialog: MatDialog) {}
+  constructor(private dialog: MatDialog, private snackBar: MatSnackBar) {}
 
   ngOnInit(): void {
     this.createAnnouncementForm = new UntypedFormGroup({
       title: new UntypedFormControl('', [Validators.required]),
-      showFrom: new UntypedFormControl('', [Validators.required]),
+      showFrom: new UntypedFormControl(new Date(), [Validators.required]),
       showTo: new UntypedFormControl('', [Validators.required]),
       comments: new UntypedFormControl('', [Validators.maxLength(1000), Validators.required]),
     });
+  }
+
+  onSubmit(): void {
+    if (!this.createAnnouncementForm.valid) {
+      if (this.createAnnouncementForm.get('title')?.invalid) {
+        this.titleInput.nativeElement.focus();
+      } else if (this.createAnnouncementForm.get('comments')?.invalid) {
+        this.commentsInput.nativeElement.focus();
+      } else if (this.createAnnouncementForm.get('showFrom')?.invalid) {
+        this.showFromInput.nativeElement.focus();
+      } else if (this.createAnnouncementForm.get('showTo')?.invalid) {
+        this.showToInput.nativeElement.focus();
+      }
+      // TODO: Add a check for files
+    } else {
+      this.createAnnouncement();
+    }
+  }
+
+  createAnnouncement(): void {
+    this.isLoading = true;
   }
 }
--- apps/frontend/src/app/pages/announcements/update-announcement-dialog/update-announcement-dialog.component.ts
+++ apps/frontend/src/app/pages/announcements/update-announcement-dialog/update-announcement-dialog.component.ts
@@ -1,21 +1,57 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { UntypedFormGroup } from '@angular/forms';
-import { MatDialog } from '@angular/material/dialog';
+import { Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
+import { UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { IAnnouncement } from '@omnihost/interfaces';
 
 @Component({
-  selector: 'frontend-update-annoucement-dialog',
+  selector: 'frontend-update-announcement-dialog',
   templateUrl: './update-announcement-dialog.component.html',
   styleUrls: ['../../../../assets/styles/dialog.scss', '../announcements.component.scss'],
 })
-export class UpdateAnnouncementDialogComponent {
+export class UpdateAnnouncementDialogComponent implements OnInit {
   updateAnnouncementForm = new UntypedFormGroup({});
   isLoading = false;
 
   @ViewChild('title') titleInput!: ElementRef;
-  @ViewChild('comments') commentInput!: ElementRef;
+  @ViewChild('comments') commentsInput!: ElementRef;
   @ViewChild('showFrom') showFromInput!: ElementRef;
   @ViewChild('showTo') showToInput!: ElementRef;
   @ViewChild('files') files!: ElementRef;
 
-  constructor(private dialog: MatDialog) {}
+  constructor(
+    private dialog: MatDialog,
+    private snackBar: MatSnackBar,
+    @Inject(MAT_DIALOG_DATA) public data: IAnnouncement
+  ) {}
+
+  ngOnInit(): void {
+    this.updateAnnouncementForm = new UntypedFormGroup({
+      title: new UntypedFormControl(this.data.title, [Validators.required]),
+      showFrom: new UntypedFormControl(this.data.showFrom, [Validators.required]),
+      showTo: new UntypedFormControl(this.data.showTo, [Validators.required]),
+      comments: new UntypedFormControl(this.data.comments, [Validators.maxLength(1000), Validators.required]),
+    });
+  }
+
+  onSubmit(): void {
+    if (!this.updateAnnouncementForm.valid) {
+      if (this.updateAnnouncementForm.get('title')?.invalid) {
+        this.titleInput.nativeElement.focus();
+      } else if (this.updateAnnouncementForm.get('comments')?.invalid) {
+        this.commentsInput.nativeElement.focus();
+      } else if (this.updateAnnouncementForm.get('showFrom')?.invalid) {
+        this.showFromInput.nativeElement.focus();
+      } else if (this.updateAnnouncementForm.get('showTo')?.invalid) {
+        this.showToInput.nativeElement.focus();
+      }
+      // TODO: Add a check for files
+    } else {
+      this.updateAnnouncement();
+    }
+  }
+
+  updateAnnouncement(): void {
+    this.isLoading = true;
+  }
 }
