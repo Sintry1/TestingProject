a137af049909fcce387fbdbc535cbbc7e312962f
Teodor25 <teodor_jonasson@hotmail.com>
2023-02-15 16:15:10 +0100

    feat: 🎸 update car notification data

--- apps/frontend/src/app/components/dashboard-widgets/car-notification-widget/car-notification-widget.component.ts
+++ apps/frontend/src/app/components/dashboard-widgets/car-notification-widget/car-notification-widget.component.ts
@@ -12,7 +12,7 @@ import { filterByCompletedAtAndOrderResults } from '../../../utils/order.util';
 export class CarNotificationWidgetComponent implements OnInit {
   carList: ICar[] = [];
   readyCarList: ICar[] = [];
-  overdueCarList: ICar[] = []
+  overdueCarList: ICar[] = [];
   sortBy: CarSortOptions = CarSortOptions.CREATED_AT;
   sortOrder: SortOrder = SortOrder.ASCENDING;
   search = '';
@@ -25,16 +25,119 @@ export class CarNotificationWidgetComponent implements OnInit {
     // being viewed in the past.
     this.carService.getCar(new Date(), this.sortBy, this.sortOrder, this.search).subscribe({
       next: (cars) => {
-        this.carList = filterByCompletedAtAndOrderResults(
-          cars,
-          false,
-          new Date()
-        );
-        this.carList = this.carList.filter((car) => !car.completedAt)
+        this.carList = filterByCompletedAtAndOrderResults(cars, false, new Date());
+        this.carList = this.carList.filter((car) => !car.completedAt);
+        this.UpdateCarListNumbers();
       },
       error: (err) => {
         console.error(err);
       },
     });
+  }
+
+  UpdateCarListNumbers(): void {
+    const THIRTY_MINUTES = 30 * 60 * 1000; // Convert 30 minutes to milliseconds
+    const now = new Date().getTime();
+
+    const futureCar: ICar = {
+      arrivalDate: new Date('2022-08-14T06:57:53.796Z'),
+      bbDown: 'future',
+      bbOut: 'future',
+      bbUp: 'future',
+      carId: '3074df38-2c0a-46e7-99c4-0401c0319d98',
+      charged: false,
+      comments: 'future',
+      completedAt: undefined,
+      createdAt: new Date('2022-08-14T06:57:53.796Z'),
+      deliveryTime: new Date('2022-08-14T14:43:53.796Z'),
+      departureDate: new Date('2022-08-14T14:43:53.796Z'),
+      expirationDate: new Date('2022-08-14T19:01:53.796Z'),
+      files: ['car.jpg'],
+      licensePlate: 'future',
+      location: 'future',
+      name: 'future',
+      parkingLot: 'future',
+      pickUpTime: new Date(new Date().getTime() + THIRTY_MINUTES * 2),
+      room: 'future',
+      tagNr: 'future',
+      updatedAt: new Date('2023-02-14T14:49:53.821Z'),
+    };
+
+    const readyCar: ICar = {
+      arrivalDate: new Date('2022-08-14T06:57:53.796Z'),
+      bbDown: 'ready',
+      bbOut: 'ready',
+      bbUp: 'ready',
+      carId: '3074df38-2c0a-46e7-99c4-0401c0319d98',
+      charged: false,
+      comments: 'ready',
+      completedAt: undefined,
+      createdAt: new Date('2022-08-14T06:57:53.796Z'),
+      deliveryTime: new Date('2022-08-14T14:43:53.796Z'),
+      departureDate: new Date('2022-08-14T14:43:53.796Z'),
+      expirationDate: new Date('2022-08-14T19:01:53.796Z'),
+      files: ['car.jpg'],
+      licensePlate: 'ready',
+      location: 'ready',
+      name: 'ready',
+      parkingLot: 'ready',
+      pickUpTime: new Date(new Date().getTime() + (THIRTY_MINUTES - 60000)),
+      room: 'ready',
+      tagNr: 'ready',
+      updatedAt: new Date('2023-02-14T14:49:53.821Z'),
+    };
+
+    const overdueCar: ICar = {
+      arrivalDate: new Date('2022-08-14T06:57:53.796Z'),
+      bbDown: 'overdue',
+      bbOut: 'overdue',
+      bbUp: 'overdue',
+      carId: '3074df38-2c0a-46e7-99c4-0401c0319d98',
+      charged: false,
+      comments: 'overdue',
+      completedAt: undefined,
+      createdAt: new Date('2022-08-14T06:57:53.796Z'),
+      deliveryTime: new Date('2022-08-14T14:43:53.796Z'),
+      departureDate: new Date('2022-08-14T14:43:53.796Z'),
+      expirationDate: new Date('2022-08-14T19:01:53.796Z'),
+      files: ['car.jpg'],
+      licensePlate: 'overdue',
+      location: 'overdue',
+      name: 'overdue',
+      parkingLot: 'overdue',
+      pickUpTime: new Date(new Date().getTime() - 180000),
+      room: 'overdue',
+      tagNr: 'overdue',
+      updatedAt: new Date('2023-02-14T14:49:53.821Z'),
+    };
+
+    const testList: ICar[] = [futureCar, readyCar, overdueCar];
+
+    this.readyCarList = this.carList.filter((car) => {
+      if (!car.pickUpTime) {
+        return false;
+      }
+      const expirationTime = new Date(car.pickUpTime).getTime();
+      // console.log(car.bbDown, expirationTime - now, THIRTY_MINUTES, (expirationTime - now) < THIRTY_MINUTES && expirationTime - now > 0);
+      // Check that the time is within 30 min        &  check that the time hasn't been passed yet
+      return expirationTime - now < THIRTY_MINUTES && expirationTime - now > 0;
+    });
+
+    this.overdueCarList = testList.filter((car) => {
+      if (!car.pickUpTime) {
+        return false;
+      }
+      const expirationTime = new Date(car.pickUpTime).getTime();
+      // console.log(car.bbDown, expirationTime < now);
+      // Check that the expirationTime current time has passed
+      if (expirationTime < now) {
+        this.carList.filter((listCar) => listCar.carId !== car.carId);
+        return true;
+      } else {
+        return false;
+      }
+    });
+
+    // console.log(this.overdueCarList);
   }
 }
