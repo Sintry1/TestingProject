ee1818311c75848c6701f336a292b40f7ee1e55a
Frederik-Vagner <70664117+Frederik-Vagner@users.noreply.github.com>
2023-02-22 13:13:08 +0100

    Update seeding data for announcements

--- apps/seeder/src/constants/announcements.constant.ts
+++ apps/seeder/src/constants/announcements.constant.ts
@@ -1,16 +1,22 @@
 export const announcements = [
   {
-    title: 'Update your schedules',
-    comments: 'Send your planned schedules for 2023 to your manager',
+    title: 'Tour De France Map Overview',
+    comments:
+      'See what part of the city, is affected by the race and learn more about how to guide the guests toward the airport',
   },
   {
-    title: 'Extra shifts available in March',
+    title: 'Balthazar New Opening Hours',
     comments:
-      'We will have extra shifts available in March for those of you that want to work a bit extra',
+      'Starting 1/3 Balthazar has expanded their opening hours on fridays, which means they now have open until 3.00',
   },
   {
-    title: 'Storm warning!',
+    title: 'New Bell Boy',
     comments:
-      'There is an forecasted storm tomorrow afternoon. Make sure to secure the equipment outside',
+      'We are happy to welcome a new bell boy to the team. Starting 1/5 Tobias Hansen is joining the team as a full-time bell boy',
+  },
+  {
+    title: 'Unexpected Guests',
+    comments:
+      'Lately we have had many problems with intruders, so be extra aware these days and look out for people who do not belong at the hotel. See pictures for example of intruders',
   },
 ];
