1546c00c2858d6d819038c421e1f8736b038ddb0
Frederik-Vagner <70664117+Frederik-Vagner@users.noreply.github.com>
2023-03-01 15:56:31 +0100

    add checkin out notification

---
+++ apps/frontend/src/app/components/dashboard-widgets/checkin-checkout-notification-widget/checkin-checkout-notification-widget.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { ILuggage, LuggageSortOptions, SortOrder } from '@omnihost/interfaces';
+import { LuggageService } from '../../../services/luggage.service';
+import { filterByCompletedAtAndOrderResults } from '../../../utils/order.util';
+
+@Component({
+  selector: 'frontend-checkin-checkout-notification-widget',
+  templateUrl: './checkin-checkout-notification-widget.component.html',
+  styleUrls: ['./checkin-checkout-notification-widget.component.scss'],
+})
+export class CheckinCheckoutNotificationWidgetComponent implements OnInit {
+  originalCheckinList: ILuggage[] = [];
+  nonCompletedCheckinList: ILuggage[] = [];
+  originalCheckoutList: ILuggage[] = [];
+  nonCompletedCheckoutList: ILuggage[] = [];
+  sortBy: LuggageSortOptions = LuggageSortOptions.CREATED_AT;
+  sortOrder: SortOrder = SortOrder.ASCENDING;
+  search = '';
+  timeTil21 = new Date();
+
+  constructor(private luggageService: LuggageService) {}
+
+  ngOnInit(): void {
+    this.fetchLuggages();
+  }
+
+  fetchLuggages(): void {
+    this.luggageService.getCheckin(new Date(), this.sortBy, this.sortOrder, this.search).subscribe({
+      next: (luggages) => {
+        this.originalCheckinList = filterByCompletedAtAndOrderResults(luggages, false, new Date());
+        this.nonCompletedCheckinList = this.originalCheckinList.filter(
+          (luggage) => !luggage.completedAt
+        );
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+    this.luggageService
+      .getCheckout(new Date(), this.sortBy, this.sortOrder, this.search)
+      .subscribe({
+        next: (luggages) => {
+          this.originalCheckoutList = filterByCompletedAtAndOrderResults(
+            luggages,
+            false,
+            new Date()
+          );
+          this.nonCompletedCheckoutList = this.originalCheckoutList.filter(
+            (luggage) => !luggage.completedAt
+          );
+        },
+        error: (err) => {
+          console.error(err);
+        },
+      });
+  }
+}
