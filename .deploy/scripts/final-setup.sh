e86a05ac9779a8f6dbe65ac6a84f8d981aa0ffcc
Kwandes <bz.jasiek@gmail.com>
2023-04-17 04:04:19 +0200

    Fix the cronjob

--- .deploy/scripts/final-setup.sh
+++ .deploy/scripts/final-setup.sh
@@ -17,10 +17,12 @@ githubRepo="https://github.com/omnihost-systems/hotel-dangleterre.git"
 
 echo "================================"
 echo "Configuring Cron jobs..."
+mkdir "/home/developer/logs"
+mkdir "/home/developer/logs/cron"
 echo "We will open a file editor. Parse the following lines into it:"
 echo "0 4 * * * date >> /home/developer/logs/cron/docker-cleanup.log"
 echo "0 4 * * * /usr/bin/docker system prune -f >> /home/developer/logs/cron/docker-cleanup.log"
-echo '0 4 * * * /home/developer/hotel-dangleterre/.deploy/scripts/seed.sh >> /home/developer/logs/cron/seeding.log"
+echo "*/5 * * * * /home/developer/hotel-dangleterre/.deploy/scripts/seed-database.sh >> /home/developer/logs/cron/seeding.log"
 read -p "Press any button once you have copied the two lines above:" temp
 crontab -e
 echo "You have set up the Cron jobs"
--- .deploy/scripts/seed-database.sh
+++ .deploy/scripts/seed-database.sh
@@ -7,10 +7,12 @@
 # This script populates the database with sample data. Only meant to be used in deployment via a cronjob
 # THIS SCRIPT WON'T WORK ON LOCAL DEVELOPMENT ENVIRONMENT!
 
-echo date >>/home/developer/logs/cron/seeding.log
+echo "======================================" >>/home/developer/logs/cron/seeding.log
+date >>/home/developer/logs/cron/seeding.log
 # Modify the env to connect to the database via localhost
 sed -i "s/API_POSTGRES_HOST=omnihost-postgres/API_POSTGRES_HOST=localhost/" /home/developer/hotel-dangleterre/.env >>/home/developer/logs/cron/seeding.log
 # Populate the database
-npm run seed >>/home/developer/logs/cron/seeding.log
+cd /home/developer/hotel-dangleterre
+/home/developer/.nvm/versions/node/v19.6.0/bin/node run seed >>/home/developer/logs/cron/seeding.log 2>&1
 # Modify the env to its previous state
 sed -i "s/API_POSTGRES_HOST=localhost/API_POSTGRES_HOST=omnihost-postgres/" /home/developer/hotel-dangleterre/.env >>/home/developer/logs/cron/seeding.log
