669e78bff412da497b944bf289a93d4b7cf550ec
Kwandes <bz.jasiek@gmail.com>
2023-04-17 16:48:24 +0200

    Refactor the seeding script and add extra logs

--- .deploy/scripts/seed-database.sh
+++ .deploy/scripts/seed-database.sh
@@ -11,11 +11,16 @@ echo "======================================" >>/home/developer/logs/cron/seedin
 date >>/home/developer/logs/cron/seeding.log
 cd /home/developer/hotel-dangleterre
 
-# Modify the env to connect to the database via localhost
+# Modify the .env to connect to the database via localhost
+echo "== Backing up the .env file and updating the API_POSTGRES_HOST property to localhost..." >>/home/developer/logs/cron/seeding.log
+cp .env .env.bak
 sed -i "s/API_POSTGRES_HOST=omnihost-postgres/API_POSTGRES_HOST=localhost/" .env >>/home/developer/logs/cron/seeding.log
 cat .env | grep API_POSTGRES_HOST >>/home/developer/logs/cron/seeding.log
 # Populate the database
+echo "== Populating the database with 'npm run seed'..." >>/home/developer/logs/cron/seeding.log
 npm run seed >>/home/developer/logs/cron/seeding.log 2>&1
-# Modify the env to its previous state
-sed -i "s/API_POSTGRES_HOST=localhost/API_POSTGRES_HOST=omnihost-postgres/" .env >>/home/developer/logs/cron/seeding.log
+# Set the .env to its previous state
+echo "== Resetting the .env file to its original state..." >>/home/developer/logs/cron/seeding.log
+mv .env.bak .env
 cat .env | grep API_POSTGRES_HOST >>/home/developer/logs/cron/seeding.log
+echo "== Seeding process completed" >>/home/developer/logs/cron/seeding.log
